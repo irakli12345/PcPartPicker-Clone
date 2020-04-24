e2df766ccc15b812e63d798433cd9d4956251731
Irakli Tchigladze <dzagli12345@gmail.com>
2020-04-24 21:08:16 +0400

    set up homepage and navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <Navbar></Navbar>
+    <ul class="homelist">
+      <li>პროცესორი (CPU) <button>ამოირჩიე</button></li>
+      <li>ვიდეო ბარათი (GPU) <button>ამოირჩიე</button></li>
+      <li>დედაბარათი (Motherboard) <button>ამოირჩიე</button></li>
+      <li>მყარი დისკი (HDD) <button>ამოირჩიე</button></li>
+      <li>ოპერატიული მეხსიერება (RAM) <button>ამოირჩიე</button></li>
     </ul>
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  components: {
+    Navbar,
+  },
+  data: function() {
+    return {
+      showCpus: false,
+      showGpus: false,
+      showMboard: false,
+      showHdd: false,
+      showRam: false,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+:root {
+  --primary: rgb(252, 81, 48);
+  --primaryh: rgb(255, 108, 96);
+  --secondary: rgb(67, 188, 205);
+  --secondaryh: rgb(156, 234, 239);
+  --tertiary: rgb(134, 222, 183);
+}
+* {
+  font-family: 'BPG Glaho WEB Caps', sans-serif;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -48,9 +52,20 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.homelist li {
+  background-color: white;
+  padding: 1rem;
+  border-bottom: 2px solid lightgrey;
+  width: 50%;
+  margin: auto;
+  margin-top: 1rem;
+}
+.homelist li button {
+  padding: 0.5rem 1rem;
+  box-shadow: none;
+  border: none;
+  background-color: var(--tertiary);
+  border-radius: 1rem;
 }
 a {
   color: #42b983;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="nav">
+    <div class="logo">
+      <a href="/"><i class="fas fa-home"></i></a>
+    </div>
+    <ul class="menu">
+      <a href="/build-a-pc"><li>კომპიუტერის აწყობა</li></a>
+      <a href="/ready-builds"><li>აწყობილი სისტემები</li></a>
+      <a href="/faq"><li>ხშირად დასმ. კითხვები</li></a>
+      <a href="contact-us"><li>კონტაქტი</li></a>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+  data: function() {},
+};
+</script>
+<style>
+:root {
+  --primary: rgb(252, 81, 48);
+  --primaryh: rgb(255, 108, 96);
+  --secondary: rgb(67, 188, 205);
+  --secondaryh: rgb(156, 234, 239);
+  --tertiary: rgb(134, 222, 183);
+}
+.menu {
+  display: flex;
+  justify-content: space-around;
+  flex-grow: 3;
+  margin: 0;
+  padding: 0;
+}
+.nav {
+  display: flex;
+  justify-content: space-around;
+  background-color: var(--primary);
+  padding: 1rem;
+}
+.logo {
+  flex-basis: 20%;
+  width: auto;
+}
+.logo a {
+  color: white;
+}
+.menu li {
+  list-style: none;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+}
+.menu li:hover {
+  background-color: var(--primaryh);
+}
+a {
+  text-decoration: none;
+}
+</style>
